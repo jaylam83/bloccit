408e46e57f0bcd64ab1d1ea84c87a3077bc1745f
jaylam83 <j.a.lam@me.com>
2015-05-02 10:00:19 -1000

    Assignment 31 complete more user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+  
+  before_action :flash_attack
+
+protected
+
+def flash_attack
+  flash[:notice]= "Flash attacking you!"
+end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+skip_before_action :flash_attack, only: [:index, :new]
+
   def index
         @posts = Post.all
   end
@@ -38,7 +40,5 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-  
-
 
 end
