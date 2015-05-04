76f4277aaad41167e4f7f8ab98a8a98e1b918a48
jaylam83 <j.a.lam@me.com>
2015-05-03 14:09:33 -1000

    Completed assignment 32 association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   
-
-  default_scope { order('created_at DESC') }
+  #default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('title ASC')}
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
 
 end
