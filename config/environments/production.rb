2a51bc3b55b887e719498f27496aaefaa08ac4dd
jaylam83 <j.a.lam@me.com>
2015-05-04 21:02:12 -1000

    Corrected production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #http://bloccitnew.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'bloccitnew.herokuapp.com' }
 end
