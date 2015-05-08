76140ffadf1f712e082ec3090ab6a1df3e9ee84a
jaylam83 <j.a.lam@me.com>
2015-05-07 23:10:54 -1000

    Complete uploading imafes assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   belongs_to :topic
+  mount_uploader :image, ImageUploader
   
 
   default_scope { order('created_at DESC') }
